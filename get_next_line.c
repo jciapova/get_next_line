db2ef3dc10bce7e324fd01a401d04573e3433e6b
Jana Ciapova <jciapova@c1r1s5.42prague.com>
2023-04-18 21:30:32 +0200

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 19:48:35 by jciapova          #+#    #+#             */
-/*   Updated: 2023/04/16 21:37:30 by ubuntu           ###   ########.fr       */
+/*   Updated: 2023/04/15 12:05:48 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,14 +69,20 @@ char	*get_next_line(int fd)
 		temp = line;
 		line = ft_strjoin(temp, buff);
 		free(temp);
-		if (ft_strchr(buff, '\n'))
-		{
-			temp = ft_strchr(line, '\n');
-			ft_strlcpy(buff, temp + 1, ft_strlen(temp + 1) + 1);
-			line = new_line(line);
-			break;
-		}
 	}
+	temp = ft_strchr(line, '\n');
+	if (temp == NULL)
+	{
+		ft_bzero(buff, BUFFER_SIZE + 1);
+		if (ft_strlen(line) == 0)
+		{
+			free(line);
+			return (NULL);
+		}
+		return (line);
+	}
+	ft_strlcpy(buff, temp + 1, ft_strlen(temp + 1) + 1);
+	line = new_line(line);
 	if (read_line == -1)
 	{	
 		free (line);
@@ -90,6 +96,18 @@ char	*get_next_line(int fd)
 // 	int     fd, i=0;
 // 	char    *line;
 // 	fd = open("test.txt",O_RDONLY);
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
 // 	printf("%s", get_next_line(fd));
 // 	printf("%s", get_next_line(fd));
 // 	printf("%s", get_next_line(fd));
